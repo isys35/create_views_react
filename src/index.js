bca1f72e373acf644d599da7b641980ecc5d400f
isys35 <isys35@mail.ru>
2021-09-24 16:08:42 +0300

    init

--- src/index.js
+++ src/index.js
@@ -3,12 +3,61 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-const executionConditions = ["команда", "представление+кнопка", "inline-кнопка"]
+const executionConditions = [
+    {title:"команда", value:'command-trigger'},
+    {title:"представление", value:'view-trigger'},
+]
 
-function CreateViewMain(props) {
-    const executionConditions = props.executionConditions;
-    const options = executionConditions.map((executionCondition) =>  <option key={executionCondition}>{executionCondition}</option>);
-    return (<div className="container">
+
+function Trigger(props) {
+    const trigger = props.trigger
+    if (trigger === 'command-trigger') {
+        return <CommandTrigger />
+    } else {
+        return <ViewTrigger />
+    }
+}
+
+function CommandTrigger(props) {
+    const commands = ['/start']
+    return (<div>
+                <span>Выберите комманду:</span>
+            </div>)
+}
+
+
+function ViewTrigger(props) {
+    const commands = ['/start']
+    return (<div>
+                <div>
+                    <span>Выберите представление:</span>
+                </div>
+            </div>
+    )
+}
+
+
+
+
+
+
+class CreateViewMain extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {trigger: executionConditions[0].value}
+    }
+
+    render() {
+        const executionConditions = this.props.executionConditions;
+        const options = executionConditions.map((executionCondition) =>
+            <option
+                key={executionCondition.title}
+                value={executionCondition.value}>
+                    {executionCondition.title}
+            </option>);
+        return (
+
+            <div className="container">
                 <h1>Окно создания представлений</h1>
                 <div>
                     <span>Выбрать условие выполнения:</span>
@@ -16,7 +65,9 @@ function CreateViewMain(props) {
                         {options}
                     </select>
                 </div>
+                <Trigger type={this.state.trigger}/>
             </div>);
+    }
 }
 
 
