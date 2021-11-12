fc117eb6b38af62265359c46ba7516ceebc24b50
isys35 <isys35@mail.ru>
2021-11-12 15:32:30 +0300

    cancel button style

--- src/index.js
+++ src/index.js
@@ -51,6 +51,37 @@ class CreateCommand extends React.Component {
 }
 
 
+function SettingsButton(props) {
+    return <div className="settings-icon" onClick={props.handleChange}>
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
+	                                    viewBox="0 0 482.568 482.568">
+                                <g>
+                                    <g>
+                                        <path d="M116.993,203.218c13.4-1.8,26.8,2.8,36.3,12.3l24,24l22.7-22.6l-32.8-32.7c-5.1-5.1-5.1-13.4,0-18.5s13.4-5.1,18.5,0
+                                            l32.8,32.8l22.7-22.6l-24.1-24.1c-9.5-9.5-14.1-23-12.3-36.3c4-30.4-5.7-62.2-29-85.6c-23.8-23.8-56.4-33.4-87.3-28.8
+                                            c-4.9,0.7-6.9,6.8-3.4,10.3l30.9,30.9c14.7,14.7,14.7,38.5,0,53.1l-19,19c-14.7,14.7-38.5,14.7-53.1,0l-31-30.9
+                                            c-3.5-3.5-9.5-1.5-10.3,3.4c-4.6,30.9,5,63.5,28.8,87.3C54.793,197.518,86.593,207.218,116.993,203.218z"/>
+                                        <path d="M309.193,243.918l-22.7,22.6l134.8,134.8c5.1,5.1,5.1,13.4,0,18.5s-13.4,5.1-18.5,0l-134.8-134.8l-22.7,22.6l138.9,138.9
+                                            c17.6,17.6,46.1,17.5,63.7-0.1s17.6-46.1,0.1-63.7L309.193,243.918z"/>
+                                        <path d="M361.293,153.918h59.9l59.9-119.7l-29.9-29.9l-119.8,59.8v59.9l-162.8,162.3l-29.3-29.2l-118,118
+                                            c-24.6,24.6-24.6,64.4,0,89s64.4,24.6,89,0l118-118l-29.9-29.9L361.293,153.918z"/>
+                                    </g>
+                                </g>
+                        </svg>
+            </div>
+}
+
+
+
+function CancelButton(props) {
+    return <div className="cancel-button">
+                <svg  id="Layer_1"   viewBox="0 0 512 512"   xmlns="http://www.w3.org/2000/svg">
+                    <path d="M437.5,386.6L306.9,256l130.6-130.6c14.1-14.1,14.1-36.8,0-50.9c-14.1-14.1-36.8-14.1-50.9,0L256,205.1L125.4,74.5  c-14.1-14.1-36.8-14.1-50.9,0c-14.1,14.1-14.1,36.8,0,50.9L205.1,256L74.5,386.6c-14.1,14.1-14.1,36.8,0,50.9  c14.1,14.1,36.8,14.1,50.9,0L256,306.9l130.6,130.6c14.1,14.1,36.8,14.1,50.9,0C451.5,423.4,451.5,400.6,437.5,386.6z"/>
+                </svg>
+           </div>
+}
+
+
 class CommandOptions extends React.Component {
     
     constructor(props) {
@@ -85,25 +116,10 @@ class CommandOptions extends React.Component {
 
     render() {
         if (this.state.isLoaded) {
+            const buttons = (this.props.type == 'select') ? <SettingsButton handleChange={this.props.handleChange}/> : <div><CancelButton /></div>;
             return (<div className="command-field">
                     <Select items={this.state.options} text={this.state.options[0].text} value={this.state.options[0].value} />
-                    <div className="settings-icon">
-                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-	                                    viewBox="0 0 482.568 482.568">
-                                <g>
-                                    <g>
-                                        <path d="M116.993,203.218c13.4-1.8,26.8,2.8,36.3,12.3l24,24l22.7-22.6l-32.8-32.7c-5.1-5.1-5.1-13.4,0-18.5s13.4-5.1,18.5,0
-                                            l32.8,32.8l22.7-22.6l-24.1-24.1c-9.5-9.5-14.1-23-12.3-36.3c4-30.4-5.7-62.2-29-85.6c-23.8-23.8-56.4-33.4-87.3-28.8
-                                            c-4.9,0.7-6.9,6.8-3.4,10.3l30.9,30.9c14.7,14.7,14.7,38.5,0,53.1l-19,19c-14.7,14.7-38.5,14.7-53.1,0l-31-30.9
-                                            c-3.5-3.5-9.5-1.5-10.3,3.4c-4.6,30.9,5,63.5,28.8,87.3C54.793,197.518,86.593,207.218,116.993,203.218z"/>
-                                        <path d="M309.193,243.918l-22.7,22.6l134.8,134.8c5.1,5.1,5.1,13.4,0,18.5s-13.4,5.1-18.5,0l-134.8-134.8l-22.7,22.6l138.9,138.9
-                                            c17.6,17.6,46.1,17.5,63.7-0.1s17.6-46.1,0.1-63.7L309.193,243.918z"/>
-                                        <path d="M361.293,153.918h59.9l59.9-119.7l-29.9-29.9l-119.8,59.8v59.9l-162.8,162.3l-29.3-29.2l-118,118
-                                            c-24.6,24.6-24.6,64.4,0,89s64.4,24.6,89,0l118-118l-29.9-29.9L361.293,153.918z"/>
-                                    </g>
-                                </g>
-                        </svg>
-                    </div>
+                    {buttons}
                     {/*<select onChange={this.handleChangeSelectedCommand}>*/}
                     {/*    {this.state.options}*/}
                     {/*</select>*/}
@@ -121,7 +137,7 @@ function SelectCommand(props) {
     return (<div className="row-select">
                 <span className="title">Комманда:</span>
                 <div>
-                    <CommandOptions setSelectedCommand={props.setSelectedCommand} handleChange={props.handleChange}/>
+                    <CommandOptions setSelectedCommand={props.setSelectedCommand} handleChange={props.handleChange} type='select'/>
                 </div>
             </div>
             )
@@ -133,11 +149,11 @@ function MainMenuChange(props) {
         <div className="row-select">
             <span className="title">Редактировать комманды:</span>
             <div>
-                <CommandOptions setSelectedCommand={props.setSelectedCommand}/>
-                <button onClick={props.handleDelete}>🗑️</button>
-                <button onClick={props.handleEdit}>✏️</button>
-                <button onClick={props.handleAdd}>➕</button>
-                <button onClick={props.handleCancelChange}>❌</button>
+                <CommandOptions setSelectedCommand={props.setSelectedCommand} type='edit'/>
+                {/*<button onClick={props.handleDelete}>🗑️</button>*/}
+                {/*<button onClick={props.handleEdit}>✏️</button>*/}
+                {/*<button onClick={props.handleAdd}>➕</button>*/}
+                {/*<button onClick={props.handleCancelChange}>❌</button>*/}
             </div>
         </div>
     )
