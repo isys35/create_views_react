136a29a40aa1337f552c2fbc63af8cfdd5a465f1
isys35 <isys35@mail.ru>
2021-11-17 17:06:52 +0300

    Select command

---
+++ src/telegram-buttons.js
@@ -0,0 +1,194 @@
+import React from 'react';
+import { HOST } from './settings';
+import { Select } from './select.js';
+
+class AddButton extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (<div className="tgbutton addbutton" onClick={this.props.setStatusCreate}>Добавить кнопку</div>)
+    }
+}
+
+
+class Button extends  React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (<div>
+                    <button className="tgbutton" disabled="disabled">{this.props.btnText}</button>
+                </div>)
+    }
+
+}
+
+class  ButtonFieldView extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        let buttons = this.props.buttons.map((button) => <Button btnText={button}/>);
+        return (<div>
+                    {buttons}
+                </div>)
+    }
+
+}
+
+
+class ButtonField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {status: 'add', buttons: []};
+        this.setStatusCreate = this.setStatusCreate.bind(this);
+        this.cancelCreate = this.cancelCreate.bind(this);
+        this.addButton = this.addButton.bind(this);
+
+    }
+
+    setStatusCreate() {
+        this.setState({status: 'create'});
+    }
+
+    addButton(text) {
+        this.setState({
+            buttons: [...this.state.buttons, text]
+        });
+    }
+
+    cancelCreate() {
+        this.setState({status: 'add'});
+    }
+
+    render() {
+        if (this.state.status == 'add') {
+            return  <div>
+                        <ButtonFieldView buttons={this.state.buttons} />
+                        <AddButton setStatusCreate={this.setStatusCreate} />
+                    </div>
+        } else {
+            return <div>
+                        <ButtonFieldView buttons={this.state.buttons} />
+                        <ButtonCreater cancelCreate={this.cancelCreate} addButton={this.addButton}/>
+                    </div>
+
+        };
+    }
+}
+
+
+class ButtonCreater extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {text: '', type: 'ReplyKeyboard', mode: 'create'};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleChangeType = this.handleChangeType.bind(this);
+        this.saveButton = this.saveButton.bind(this);
+        this.setSelectMode = this.setSelectMode.bind(this);
+        this.setCreateMode = this.setCreateMode.bind(this);
+    }
+    
+    
+    handleChange(event) {
+        this.setState({text: event.target.value});
+    }
+
+    handleChangeType(event) {
+        this.setState({type: event.target.value});
+    }
+
+    setSelectMode() {
+        this.setState({mode: 'select'});
+    }
+
+    setCreateMode() {
+        this.setState({mode: 'create'});
+    }
+
+
+    saveButton() {
+        const url = (this.props.typeButtons === 'ReplyKeyboard') ? `${HOST}replybuttons` :  `${HOST}inlinebuttons`;
+         fetch(url,
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    value: this.state.text
+                    }),
+                headers: {"Content-type": "application/json; charset=UTF-8"}})
+            .then(async res => {
+                let result = await res.json();
+                if (!res.ok) {
+                    console.log(result.detail);
+                } else {
+                    this.props.addButton(this.state.text);
+                    this.props.cancelCreate();
+                }
+            })
+    }
+
+
+    render() {
+        const inputBlock = (this.state.mode === 'create') ? <input onChange={this.handleChange} type="text" value={this.state.text}/> : <button></button>
+        const changeModeBlock = (this.state.mode === 'create') ? <button onClick={this.setSelectMode}>🔎</button> : <button onClick={this.setCreateMode}>✏️</button>
+        return (
+            <div className="button-creator">
+                <Button btnText={this.state.text}/>
+                {inputBlock}
+                <button onClick={this.saveButton}>💾</button>
+                <button onClick={this.props.cancelCreate}>❌</button>
+                {changeModeBlock}
+            </div>
+    )
+    }
+
+}
+
+
+class SelectButtonType extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {status: 'active'};
+    }
+
+    render() {
+        return <div className="row-select">
+                    <span className="title">Тип кнопок: </span>
+                    <Select items={this.props.buttonTypes} text={this.props.buttonTypes[0].text} value={this.props.buttonTypes[0].value} handleChange = {this.props.onChangeType}/>
+                </div>
+
+    }
+
+}
+
+
+export class ButtonView extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {typeButtons: 'ReplyKeyboard'}
+        this.changeType = this.changeType.bind(this);
+    }
+
+    changeType(type) {
+        this.setState(
+            {typeButtons: type}
+        )
+    }
+
+    render() {
+        const buttonTypes = [
+            {value: 'ReplyKeyboard', text:'ReplyKeyboard'},
+            {value: 'InlineKeyboard', text:'InlineKeyboard'}
+        ];
+        const buttonField = (this.state.typeButtons == 'ReplyKeyboard') ? <div><ButtonField typeButtons={this.state.typeButtons} /></div> : <div></div>;
+        return (
+            <div>
+                <SelectButtonType buttonTypes={buttonTypes} onChangeType={this.changeType} />
+                { buttonField }
+            </div>
+    )
+    }
+}
