44a64645726eb0da6033b92a7a95235f3b2420ab
isys35 <isys35@mail.ru>
2021-12-20 17:25:41 +0300

    add main-menu

---
+++ src/components/bot-settings/bot-settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class BotSettings extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>Настройки ботов</h1>
+    }
+}
--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ import { TextView } from './components/text-view'
 import { SelectTrigger } from  './components/trigger';
 import { Trigger } from  './components/trigger';
 import { HOST } from './settings';
+import { BotSettings } from './components/bot-settings/bot-settings'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 const executionConditions = [
     {text:"команда", value:'command-trigger'},
@@ -15,6 +22,24 @@ const executionConditions = [
 ]
 
 
+class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <nav className="main-menu">
+                <h2 className="main-menu__header">Меню</h2>
+                <ul className="main-menu__list">
+                    <li className="main-menu__list-item"><a href="/">Главное меню</a></li>
+                    <li className="main-menu__list-item"><a href="/create-step">Создание шага</a></li>
+                    <li className="main-menu__list-item"><a href="/bot-settings">Настройка ботов</a></li>
+                </ul>
+            </nav>
+        )
+    }
+}
 
 
 
@@ -91,6 +116,7 @@ class CreateViewMain extends React.Component {
     createStep() {
         if (this.validate()) {
             console.log(this.state.input_id);
+            console.log(this.state.text);
         }
     }
 
@@ -115,7 +141,22 @@ class CreateViewMain extends React.Component {
 }
 
 
+class App extends React.Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Menu />
+                <Routes>
+                    <Route path="create-step" element={<CreateViewMain executionConditions={executionConditions}/>}></Route>
+                    <Route path="bot-settings" element={<BotSettings />}></Route>
+                </Routes>
+            </BrowserRouter>
+        )
+    }
+}
+
+
 ReactDOM.render(
-  <CreateViewMain executionConditions={executionConditions} />,
+  <App />,
   document.getElementById('root')
 );
